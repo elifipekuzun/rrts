6e75e771bd2cd845ada4e69789d816a3bcecdbe5
Elif  ipek <110392284+elifipekuzun@users.noreply.github.com>
2022-08-28 18:19:10 +0300

    adding new files

---
+++ src/reducers/todos.ts
@@ -0,0 +1,14 @@
+import { Todo, ActionTypes, Action } from '../actions';
+
+export const todosReducer = (state: Todo[] = [], action: Action) => {
+  switch (action.type) {
+    case ActionTypes.fetchTodos:
+      return action.payload;
+    case ActionTypes.deleteTodo:
+      const id = action.payload;
+      const updatedTodos = state.filter((todo: Todo) => todo.id !== id);
+      return updatedTodos;
+    default:
+      return state;
+  }
+};
